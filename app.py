a551d2766c34727b589baecda3ab6f9b139fe55c
IgnacioCantoni <ignaciomatiascantoni2001@gmail.com>
2026-05-09 17:46:13 -0300

    Eliminacion de columnas duplicadas y limpieza de nombres

--- app.py
+++ app.py
@@ -9,3 +9,17 @@ print("\nDATASET ORIGINAL")
 
 print(f"Filas originales: {df.shape[0]}")
 print(f"Columnas originales: {df.shape[1]}")
+
+# ELIMINAR COLUMNAS DUPLICADAS
+df = df.loc[:, ~df.columns.duplicated()]
+
+# LIMPIEZA DE LOS NOMBRES DE LAS COLUMNAS
+df.columns = (
+    df.columns
+    .str.strip()
+    .str.lower()
+    .str.replace(" ", "_")
+    .str.replace(r"[^a-zA-Z0-9_]", "", regex=True)
+)
+
+print("\nColumnas estandarizadas correctamente.")
